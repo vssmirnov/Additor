8740aedf3fe0603d32718cf63d114d750ff02e3c
Vladimir Smirnov <funkrailroad@Funks-Mac-Pro.local>
2016-08-15 10:04:31 -0400

    additive synth refactored with class syntax

--- src/scripts/AdditiveSynth.js
+++ src/scripts/AdditiveSynth.js
@@ -3,41 +3,37 @@ define(function(require){
 
     var AdditiveSynthVoice = require('./AdditiveSynthVoice');
 
-    var AdditiveSynth = function AdditiveSynth(o){
-        o = o || {};
+    class AdditiveSynth {
+      constructor (o) {
+          o = o || {};
 
-        // each generator node has a gain and a panner
-        // audio path: source -> output -> panner -> connect(destination)
-        this.output = window.audioCtx.createGain();
-        this.output.gain.value = o.gain || 1;
-        this.panner = window.audioCtx.createStereoPanner();
-        this.output.connect(this.panner);
-        this.panner.connect(o.destination || window.audioCtx.destination);
+          // each generator node has a gain and a panner
+          // audio path: source -> output -> panner -> connect(destination)
+          this.output = window.audioCtx.createGain();
+          this.output.gain.value = o.gain || 1;
+          this.panner = window.audioCtx.createStereoPanner();
+          this.output.connect(this.panner);
+          this.panner.connect(o.destination || window.audioCtx.destination);
 
-        this.numberOfVoices = o.numberOfVoices || 1;
+          this.numberOfVoices = o.numberOfVoices || 1;
 
-        this.voices = [];
-        this.voices[0] = {};
-        for(var i = 1; i <= this.numberOfVoices; i++) {
-            var newAdditiveSynthVoice = new AdditiveSynthVoice(o);
-            newAdditiveSynthVoice.connect(this.output);
-            this.voices.push(newAdditiveSynthVoice);
-        }
-    };
+          this.voices = [];
+          this.voices[0] = {};
+          for(var i = 1; i <= this.numberOfVoices; i++) {
+              var newAdditiveSynthVoice = new AdditiveSynthVoice(o);
+              newAdditiveSynthVoice.connect(this.output);
+              this.voices.push(newAdditiveSynthVoice);
+          }
+      }
 
-    /**
-     * TODO: these are placeholders for tests
-     */
-    AdditiveSynth.prototype.playADSR = function playADSR(o) {
+      playADSR (o) {
         this.voices[1].playADSR(o);
-    };
+      }
 
-    /**
-     * TODO: these are placeholders for tests
-     */
-    AdditiveSynth.prototype.releaseADSR = function releaseADSR(o) {
+      releaseADSR (o) {
         this.voices[1].releaseADSR(o);
-    };
+      }
+    }
 
     return AdditiveSynth;
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,25 @@
 define(function(require) {
-    'use strict';
+  'use strict';
 
-    /* --- require the modules --- */
-      var AdditiveSynth = require('./AdditiveSynth');
-      var LiveKeyboard = require('./LiveKeyboard');
-      var LiveDial = require('./LiveDial');
-      var LiveSlider = require('./LiveSlider');
+  /* --- require the modules --- */
+    var AdditiveSynth = require('./AdditiveSynth');
+    var LiveKeyboard = require('./LiveKeyboard');
+    var LiveDial = require('./LiveDial');
+    var LiveSlider = require('./LiveSlider');
 
-    /* --- DOM Containers --- */
-      var kbdContainer = document.getElementById('additor-kbd-container');
-      var volumeDialContainer = document.getElementById('volume-dial-container');
-      var sliderContainer = document.getElementById('slider-container');
+  /* --- DOM Containers --- */
+    var kbdContainer = document.getElementById('additor-kbd-container');
+    var volumeDialContainer = document.getElementById('volume-dial-container');
+    var sliderContainer = document.getElementById('slider-container');
 
-    /* --- UI Elements --- */
-      var additorKbd = new LiveKeyboard(kbdContainer);
-      var volumeDial = new LiveDial(volumeDialContainer);
-      var slider = new LiveSlider(sliderContainer);
+  /* --- UI Elements --- */
+    var additorKbd = new LiveKeyboard(kbdContainer);
+    var volumeDial = new LiveDial(volumeDialContainer);
+    var slider = new LiveSlider(sliderContainer);
 
-    /* --- Synth --- */
-      var additorSynth = new AdditiveSynth({
-          numberOfHarmonics: 200
-      });
+  /* --- Synth --- */
+    var additorSynth = new AdditiveSynth({
+        numberOfHarmonics: 200
+    });
+
 });
