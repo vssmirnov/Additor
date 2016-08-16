1817f933050fc2d69c102b9eb22d1a9fd98c0d04
Vladimir Smirnov <funkrailroad@Funks-Mac-Pro.local>
2016-08-16 16:21:24 -0400

    refactor GeneratorVoice

--- src/scripts/GeneratorVoice.js
+++ src/scripts/GeneratorVoice.js
@@ -1,45 +1,85 @@
 define(function (require) {
     'use strict';
 
-    // Create an Audio Context if one does not already exist
-    if(!window.audioCtx) {
-        var AudioContext = window.AudioContext || window.webkitAudioContext;
-        window.audioCtx = new AudioContext();
-    }
+    class GeneratorVoice {
+      constructor (o) {
+        o = o || {};
 
-    /**
-     * A GeneratorVoice module represents a generic audio generator module
-     * @param {Object} o - options
-     */
-    var GeneratorVoice = function (o) {
-        var o = o || {};
+        this._audioCtx = o.audioCtx || window.audioCtx || new AudioContext();
 
-        // each generator node has a basic audio channel strip with gain and a panner
-        // _source -> _output -> _panner -> connect(destination)
-        this.output = window.audioCtx.createGain();
-        this.panner = window.audioCtx.createStereoPanner();
-        this.output.connect(this.panner);
-    };
+        this._pan = o.pan || 0; // -1: hard left, 1: hard right
+        this._gain = o.gain || 1;
 
-    /**
-     * Connect the output of this voice to a specified destination
-     * @param {Object} destination
-     */
-    GeneratorVoice.prototype.connect = function connect(destination) {
-        this.panner.connect(destination);
-    }
+        /**
+         * channelStrip = {
+         *    panner: stereoPannerNode,
+         *    output: gainNode
+         * }
+         */
+        this._channelStrip = this.createChannelStrip();
+        this._output = this._channelStrip.output;
+      }
 
-    /**
-     * Get/set the pan for the individual voice
-     * @param {Number} pan - -1 is left, 0 is center, 1 is right
-     */
-    GeneratorVoice.prototype.pan = function pan(pan) {
-        if(!pan) {
-            return this.panner.pan;
-        } else {
-            this.panner.pan.value = pan;
-            return this;
-        }
+      /* --- getters and setters --- */
+
+      set options (o) {
+        this.audioCtx = o.audioCtx || this.audioCtx;
+        this.gain = o.gain || this.gain;
+        this.pan = o.pan || this.pan;
+      }
+
+      get options () {
+        return {}
+      }
+
+      set audioCtx (newAudioCtx) {
+        this._audioCtx = newAudioCtx;
+        return this;
+      }
+
+      get audioCtx () {
+        return this._audioCtx;
+      }
+
+      set gain (newGain) {
+        this._gain = newGain;
+        this._channelStrip.output.gain.value = this._gain;
+        return this;
+      }
+
+      get gain () {
+        return this._gain;
+      }
+
+      set pan (newPan) {
+        this._pan = newPan;
+        this._channelStrip.panner.pan.value = this._pan;
+        return this;
+      }
+
+      get pan () {
+        return this._pan;
+      }
+      
+      /* --- audio setup methods --- */
+
+      createChannelStrip () {
+        var newChannelStrip = {};
+        var newPanner = this._audioCtx.createStereoPanner();
+        var newOutput = this._audioCtx.createGain();
+
+        newPanner.pan.value = this._pan || 0;
+        newOutput.gain.value = this._gain || 1;
+
+        newPanner.connect(newOutput);
+
+        newChannelStrip = {
+          panner: newPanner,
+          output: newOutput
+        };
+
+        return newChannelStrip;
+      }
     }
 
     return GeneratorVoice;
