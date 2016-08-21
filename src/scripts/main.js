56ff95ae564f461ae2afa58efcd512e29d149b9b
Vladimir Smirnov <funkrailroad@Funks-Mac-Pro.local>
2016-08-21 13:02:23 -0400

    syncing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,30 +3,35 @@ define(function(require) {
 
   /* --- require the modules --- */
     var Overtone = require('./Overtone');
-    var AdditiveSynthVoice = require('./AdditiveSynthVoice_new');
+    var AdditiveSynthVoice = require('./AdditiveSynthVoice');
     var AdditiveSynth = require('./AdditiveSynth');
     var LiveKeyboard = require('./LiveKeyboard');
     var LiveDial = require('./LiveDial');
     var LiveSlider = require('./LiveSlider');
 
   /* --- DOM Containers --- */
-    var kbdContainer = document.getElementById('additor-kbd-container');
+    var additorKbdContainer = document.getElementById('additor-kbd-container');
     var volumeDialContainer = document.getElementById('volume-dial-container');
-    var sliderContainer = document.getElementById('slider-container');
+    var volumeSliderContainer = document.getElementById('volume-slider-container');
 
   /* --- UI Elements --- */
-    var additorKbd = new LiveKeyboard(kbdContainer);
+    var additorKbd = new LiveKeyboard(additorKbdContainer, {
+      bottomNote: 24,
+      topNote: 71
+    });
     var volumeDial = new LiveDial(volumeDialContainer);
-    var slider = new LiveSlider(sliderContainer);
+    var volumeSlider = new LiveSlider(volumeSliderContainer, {
+      displayName: 'Volume'
+    });
 
   /* --- Synth --- */
     var additorVoice = new AdditiveSynthVoice();
     var playing = false;
 
     additorVoice.connect(additorVoice._audioCtx.destination);
-    additorVoice.gain = 0.1;
-    
-    document.onclick = () => {
+    volumeSlider.subscribe(this, val => { additorVoice.gain = val / 127 });
+
+    kbdContainer.onclick = () => {
       if(playing === false) {
         additorVoice.play();
       } else {
