d82c24d945b8031b82712b6ef0e9b919872b99e3
Rimidalv Vonrimz <railroad@Rimidalvs-Pro.home>
2017-05-16 19:32:40 -0400

    abstracting controller hierarchy

--- src/controllers/AudioCtrl.js
+++ src/controllers/AudioCtrl.js
@@ -1,15 +1,15 @@
-/* ------------------------ */
-/* --- Audio controller --- */
-/* ------------------------ */
-
 import ChannelStrip from '../synth/ChannelStrip';
 import StereoFeedbackDelay from '../synth/StereoFeedbackDelay';
 import AdditiveSynth from '../synth/AdditiveSynth';
 
 'use strict';
 
+/**
+ *  Audio controller defines the WebAudio context, audio nodes, and their connections
+ */
 const AudioCtrl = function AudioCtrl(adt) {
 
+  // detect the AudioContext
   if (typeof AudioContext !== 'undefined') {
     adt.audioCtx = new AudioContext();
   } else if (typeof webkitAudioContext !== 'undefined') {
@@ -18,18 +18,22 @@ const AudioCtrl = function AudioCtrl(adt) {
     alert('Error: no Web Audio API detected in this browser');
   }
 
+  // set up the channel strip for the main output
   adt.output.node = new ChannelStrip({ audioCtx: adt.audioCtx });
   adt.output.node.connect(adt.audioCtx.destination);
 
+  // set up the stereo feedback delay module
   adt.delay.node = new StereoFeedbackDelay({
     audioCtx: adt.audioCtx,
     maxDelayTime: 10
   });
   adt.delay.node.connect(adt.output.node.input);
 
+  // set up the filter module
   adt.filter.node = adt.audioCtx.createBiquadFilter();
   adt.filter.node.connect(adt.delay.node.input);
 
+  // set up the additive synth generator module
   adt.synth.node = new AdditiveSynth({
     audioCtx: adt.audioCtx,
     numVoices: 8,
--- src/controllers/KeyboardCtrl.js
+++ src/controllers/KeyboardCtrl.js
@@ -1,32 +1,36 @@
-/* --------------------------- */
-/* --- Keyboard controller --- */
-/* --------------------------- */
-
 import Keyboard from '../widgets/Keyboard';
 
 'use strict';
 
-const KeyboardCtrl = function KeyboardCtrl(adt) {
+/**
+ * Keyboard controller
+ * @param {Object} topCtrl - The top-level controller that manages all widgets for this synth
+ * @return {Object} keyboardCtrl - the keyboard controller
+ */
+const KeyboardCtrl = function KeyboardCtrl(topCtrl) {
 
-    adt.kbd = new Keyboard({
+    // create a new keyboard widget and place it in the specified container
+    let kbdWidget = new Keyboard({
         container: document.querySelector('#additor .kbd-ctrl .kbd'),
         bottomNote: 12,
         topNote: 72,
         mode: 'polyphonic',
         blackKeyColor: '#242424'
-      })
-      .subscribe(this, (kbdNoteEvent) => {
+    });
+
+    // subscribe a function to handle the note-on and note-off events emitted by the keyboard widget
+    kbdWidget.subscribe(this, (kbdNoteEvent) => {
         var pitch = kbdNoteEvent.pitch;
         var vel = kbdNoteEvent.velocity;
 
         if(vel === 0) {
-          adt.synth.node.releaseNote(pitch);
+          topCtrl.synth.node.releaseNote(pitch);
         } else {
-          adt.synth.node.playNote(pitch);
+          topCtrl.synth.node.playNote(pitch);
         }
     });
 
-    return adt.kbd;
+    return kbdWidget;
 };
 
 export default KeyboardCtrl
