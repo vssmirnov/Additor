29ef074ccaef8da270b65ab38b452377d2295623
vsm22 <vssmirnov@gmail.com>
2017-05-31 15:00:43 -0400

    removed requireJS and related files

--- test/DestinationAudioModuleSpec.js
+++ test/DestinationAudioModuleSpec.js
@@ -1,3 +1,4 @@
+import AudioModule from '../src/audio_modules/AudioModule';
 import DestinationAudioModule from '../src/audio_modules/DestinationAudioModule';
 
 describe("DestinationAudioModule", function() {
@@ -9,6 +10,22 @@ describe("DestinationAudioModule", function() {
   });
 
   it("should initialize a new DestinationAudioModule object", function() {
-    expect(destinationAudioModule.constructor.name).toEqual("DestinationAudioModule");
+    expect(destinationAudioModule).toBeDefined();
+  });
+
+  it("should have and input property", function() {
+    expect(destinationAudioModule.input).toBeDefined();
+  });
+
+  it("should have an output property", function() {
+    expect(destinationAudioModule.output).toBeDefined();
+  });
+
+  it("should have it's input property constructor to be GainNode", function() {
+    expect(destinationAudioModule.input.constructor.name).toEqual("GainNode");
+  });
+
+  it("should have it's output property constructor to be GainNode", function() {
+    expect(destinationAudioModule.output.constructor.name).toEqual("GainNode");
   });
 });
