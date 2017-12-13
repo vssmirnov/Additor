4ee8892101f7024e15fc95f18ab6cda9acb057ca
vlad <vssmirnov@gmail.com>
2017-12-13 14:01:49 -0500

    Implement output controller

--- src/controller_modules/output-controller.js
+++ src/controller_modules/output-controller.js
@@ -1,121 +1,112 @@
-import Slider from 'ui/slider';
-import Dial from 'ui/dial';
-import Numberbox from 'ui/numberbox';
-import Meter from 'ui/meter';
+import ControllerModule from "controller_modules/controller-module";
+import Slider from "ui/slider";
+import Dial from "ui/dial";
+import Numberbox from "ui/numberbox";
+import Meter from "ui/meter";
 
 'use strict';
 
 /**
- * Main output controllers
- * @param {object} dependencies
- *  Required Dependencies
- *    Audio Modules:
- *      "output-audio-module"
- *      "audio-context"
- *    DOM References:
- *      "pan-dial-container"
- *      "pan-numbox-container"
- *      "volume-slider-container"
- *      "volume-numbox-container"
- *      "output-meter-l-container"
- *      "output-meter-r-container"
+ * Class representing a controller linking an Output UI with an Audio Module.
+ * @class
+ * @implements {ControllerModule}
  */
-let MainOutputCtrl = function MainOutputCtrl(dependencies) {
+class OutputController extends ControllerModule {
 
-    // audio module references
-    const AUDIO_CTX = dependencies["audio-context"];
-    const OUTPUT_AUDIO_MODULE = dependencies["output-audio-module"];
+  /**
+   * @constructor
+   * @param {object} dep - Dependencies.
+   * @param {object} dep["audio-context"];
+   * @param {object} dep["output-audio-module"];
+   * @param {object} dep["pan-dial-container"];
+   * @param {object} dep["pan-numbox-container"];
+   * @param {object} dep["volume-slider-container"];
+   * @param {object} dep["volume-numbox-container"];
+   * @param {object} dep["output-meter-l-container"];
+   * @param {object} dep["output-meter-r-container"];
+   * @param {object} o - Options.
+   */
+  constructor(dep, o) {
+    super();
 
-    // DOM references
-    const PAN_DIAL_CONTAINER = dependencies["pan-dial-container"];
-    const PAN_NUMBOX_CONTAINER = dependencies["pan-numbox-container"];
-    const VOLUME_SLIDER_CONTAINER = dependencies["volume-slider-container"];
-    const VOLUME_NUMBOX_CONTAINER = dependencies["volume-numbox-container"];
-    const OUTPUT_METER_L_CONTAINER = dependencies["output-meter-l-container"];
-    const OUTPUT_METER_R_CONTAINER = dependencies["output-meter-r-container"];
+    const _this = this;
 
-    // placeholder for the controller object components
-    let outputCtrl = {
-      panDial: {},
-      panNumbox: {},
-      volumeSlider: {},
-      volumeNumbox: {}
-    }
+    o = o || {};
 
-    // pan dial
-    outputCtrl.panDial = new Dial({
-        container: PAN_DIAL_CONTAINER,
-        value: Math.trunc(OUTPUT_AUDIO_MODULE.pan * 100),
-        minValue: -100,
-        maxValue: 100
-      })
-      .subscribe(this, (val) => {
-        OUTPUT_AUDIO_MODULE.pan = val / 100;
+    this.AUDIO_CTX = dep["audio-context"];
+    this.OUTPUT_AUDIO_MODULE = dep["output-audio-module"];
+    this.PAN_DIAL_CONTAINER = dep["pan-dial-container"];
+    this.PAN_NUMBOX_CONTAINER = dep["pan-numbox-container"];
+    this.VOLUME_SLIDER_CONTAINER = dep["volume-slider-container"];
+    this.VOLUME_NUMBOX_CONTAINER = dep["volume-numbox-container"];
+    this.OUTPUT_METER_L_CONTAINER = dep["output-meter-l-container"];
+    this.OUTPUT_METER_R_CONTAINER = dep["output-meter-r-container"];
+
+    this.uiElements = {
+
+      panDial: new Dial(_this.PAN_DIAL_CONTAINER, 
+        Object.assign({}, o, {
+          minVal: -100,
+          maxVal: 100
+        })
+      ),
+
+      volumeSlider: new Slider(_this.VOLUME_SLIDER_CONTAINER, 
+        Object.assign({}, o, {
+          minVal: 0,
+          maxVal: 127
+        })
+      ),
+
+      volumeNumbox: new Numberbox(_this.VOLUME_NUMBOX_CONTAINER, 
+        Object.assign({}, o, {
+          minVal: -24,
+          maxVal: 7,
+          appendString: ' dB'
+        })
+      ),
+
+      outputMeterL: new Meter(_this.OUTPUT_METER_L_CONTAINER, _this.AUDIO_CTX),
+
+      outputMeterR: new Meter(_this.OUTPUT_METER_R_CONTAINER, _this.AUDIO_CTX)
+    };
+
+    this.observers = {
+
+      panDial: function panDial(val) {
+        _this.OUTPUT_AUDIO_MODULE.pan = val / 100;
 
         if (val === 0) {
-          outputCtrl.panNumbox.appendString = ' (C)';
+          _this.panNumbox.setOptions({ appendString: ' (C)'});
         } else if (val < 0) {
-          outputCtrl.panNumbox.appendString = ' % L';
-          outputCtrl.panNumbox.value = Math.abs(val);
+          _this.panNumbox.setOptions({ appendString: ' % L'});
+          _this.panNumbox.setInternalVal(Math.abs(val));
         } else {
-          outputCtrl.panNumbox.appendString = ' % R';
-          outputCtrl.panNumbox.value = Math.abs(val);
+          _this.panNumbox.setOptions({ appendString: ' % R'});
+          _this.panNumbox.setInternalVal(Math.abs(val));
         }
-    });
+      },
 
-    // pan num box
-    outputCtrl.panNumbox = new Numberbox({
-        container: PAN_NUMBOX_CONTAINER,
-        value: Math.trunc(OUTPUT_AUDIO_MODULE.pan * 100),
-        minValue: -100,
-        maxValue: 100,
-        appendString: ' (C)'
-      })
-      .subscribe(this, (val) => {
-        OUTPUT_AUDIO_MODULE.pan = val / 100;
-        outputCtrl.panDial.value = val;
-    });
+      volumeSlider: function volumeSlider(val) {
+        _this.OUTPUT_AUDIO_MODULE.outputGain = val / 100;
+        _this.volumeNumbox.setInternalVal(((val / 100) * 24) - 24);
+      },
 
-    // volume slider
-    outputCtrl.volumeSlider = new Slider({
-        container: VOLUME_SLIDER_CONTAINER,
-        value: OUTPUT_AUDIO_MODULE.outputGain * 100,
-        minValue: 0,
-        maxValue: 127
-      })
-      .subscribe(this, (val) => {
-        OUTPUT_AUDIO_MODULE.outputGain = val / 100;
-        outputCtrl.volumeNumbox.value = ((val / 100) * 24) - 24;
-    });
+      volumeNumbox: function volumeNumbox(val) {
+        _this.OUTPUT_AUDIO_MODULE.outputGain = ((val + 24) / 31);
+        _this.volumeSlider.setInternalVal(((val + 24) / 31) * 127);
+      }
+    };
 
-    // volume numbox
-    outputCtrl.volumeNumbox = new Numberbox({
-        container: VOLUME_NUMBOX_CONTAINER,
-        value: OUTPUT_AUDIO_MODULE.outputGain,
-        minValue: -24,
-        maxValue: 7,
-        appendString: ' dB'
-      })
-      .subscribe(this, (val) => {
+    this.CHANNEL_SPLITTER = this.AUDIO_CTX.createChannelSplitter(2);
+    this.OUTPUT_AUDIO_MODULE.connect(this.CHANNEL_SPLITTER);
+    this.CHANNEL_SPLITTER.connect(this.uiElements.outputMeterL.input, 0);
+    this.CHANNEL_SPLITTER.connect(this.uiElements.outputMeterR.input, 1);
 
-    });
+    this._registerObservers();
+  }
 
-    // output meter
-    const OUTPUT_METER_L = new Meter({
-      audioCtx: AUDIO_CTX,
-      container: OUTPUT_METER_L_CONTAINER
-    });
-    const OUTPUT_METER_R = new Meter({
-      audioCtx: AUDIO_CTX,
-      container: OUTPUT_METER_R_CONTAINER
-    });
+  updateUI() {}
+}
 
-    const CHANNEL_SPLITTER = AUDIO_CTX.createChannelSplitter(2);
-    OUTPUT_AUDIO_MODULE.connect(CHANNEL_SPLITTER);
-    CHANNEL_SPLITTER.connect(OUTPUT_METER_L.input, 0);
-    CHANNEL_SPLITTER.connect(OUTPUT_METER_R.input, 1);
-
-    return outputCtrl;
-};
-
-export default MainOutputCtrl
+export default OutputController;
