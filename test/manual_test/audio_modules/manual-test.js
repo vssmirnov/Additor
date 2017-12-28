cbfb27d8b0f2c7f40ea42ab41866d6b535f184d6
vlad <vssmirnov@gmail.com>
2017-12-28 16:06:33 -0500

    Add test for StereoPannerShim

--- test/manual_test/audio_modules/manual-test.js
+++ test/manual_test/audio_modules/manual-test.js
@@ -3,14 +3,49 @@ import StereoPannerShim from "audio_modules/stereo-panner-shim";
 
 import Dial from "ui/dial";
 
-var panDial = new Dial(document.getElementById("pan-dial"), {
-  minVal: -1,
-  maxVal: 1,
-  stepInterval: 0.01
-});
+const AUDIO_CTX = new AudioContext();
+const DEST = AUDIO_CTX.destination;
 
-panDial.addObserver(val => {
-  console.log(val);
-});
+/* ============================================================================================= */
+/* STEREO PANNER SHIM TEST
+/* ============================================================================================= */
+
+(function() {
+
+  AUDIO_CTX.createStereoPanner = function() { return new StereoPannerShim(AUDIO_CTX); };
+
+  let stereoPanner = AUDIO_CTX.createStereoPanner();
+  let oscillator = AUDIO_CTX.createOscillator();
+  let gain = AUDIO_CTX.createGain();
+
+  oscillator.connect(stereoPanner);
+  stereoPanner.connect(gain);
+  gain.connect(DEST);
+
+  gain.gain.value = 0;
+
+  oscillator.start();
+
+  // pan dial
+  var panDial = new Dial(document.getElementById("pan-dial"), {
+    minVal: -1,
+    maxVal: 1,
+    stepInterval: 0.01
+  });
+
+  panDial.addObserver(val => {
+    stereoPanner.pan.value = val;
+  });
+
+  // audio on/off toggle
+  document.getElementById("stereo-panner-audio-toggle").addEventListener("change", function(ev) {
+    if (ev.target.checked) {
+      gain.gain.value = 0.5;
+    } else {
+      gain.gain.value = 0;
+    }
+  });
+
+}());
 
 
